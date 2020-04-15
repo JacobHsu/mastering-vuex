d087f2a26d6c9541ec05ac93c20d95ce6adf203f
jacobhsu <jacob.hsu.tw@gmail.com>
2020-04-15 18:10:41 +0800

    State & Getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>there are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+// we can name our mapState, or not
+export default {
+  computed: {
+    localComputed() {
+      return something
+    },
+    ...mapGetters(['getEventById', 'catLength']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/EventCreate0.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>Create Event, {{ $store.state.user}}</h3>
+    <h3>computed: {{ userName}} </h3>
+    <h2>Create id, {{ user.id}}</h2>
+    <h2>Create name, {{ user.name}}</h2>
+  </div>
+</template>
+
+<script>
+// mapState into Compputed Properters
+import { mapState } from 'vuex'
+
+export default {
+  // computed: {
+  //   userName () {
+  //     return this.$store.state.user.name
+  //   }
+  // }
+  computed: mapState({
+    userName: state => state.user.name,
+    userID: state => state.user.id,
+    categories: 'categories', //state => state.user.categories
+    user: 'user'
+  })
+}
+</script>
+
+<style scoped>
+</style>
